cc3bea6e72433ef00d290191c2620f26b3f2e89d
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:18 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/herbalism/component/PredefinedHerbComponent.java
+++ src/main/java/org/terasology/herbalism/component/PredefinedHerbComponent.java
@@ -1,26 +1,18 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.herbalism.component;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * Add this component to an item to indicate that it is a predefined herb. That is, a prefab was directly used to generate
  * this.
  */
-public final class PredefinedHerbComponent implements Component {
+public final class PredefinedHerbComponent implements Component<PredefinedHerbComponent> {
     public String herbBaseGenome;
+
+    @Override
+    public void copy(PredefinedHerbComponent other) {
+        this.herbBaseGenome = other.herbBaseGenome;
+    }
 }
